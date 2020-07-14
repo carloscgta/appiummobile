6563901e2a699df99d73867f632fff27fecafa75
Carlos Almeida <60942979+carlosteixeira89@users.noreply.github.com>
2020-07-14 10:15:53 -0300

    Initial commit

---
+++ src/main/java/br/com/icarrositau/appium/App.java
@@ -0,0 +1,13 @@
+package br.com.icarrositau.appium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
