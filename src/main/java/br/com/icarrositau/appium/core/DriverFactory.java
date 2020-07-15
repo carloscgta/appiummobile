bf390e9a7563b7201d431cdb261f9de15d1bc88e
Carlos Almeida <60942979+carlosteixeira89@users.noreply.github.com>
2020-07-15 18:12:08 -0300

    unable to locate ADB ....

--- src/main/java/br/com/icarrositau/appium/core/DriverFactory.java
+++ src/main/java/br/com/icarrositau/appium/core/DriverFactory.java
@@ -4,9 +4,12 @@ import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.openqa.selenium.DeviceRotation;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.objenesis.strategy.PlatformDescription;
 
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
@@ -18,23 +21,23 @@ import io.appium.java_client.remote.MobilePlatform;
 
 public class DriverFactory {
 
-	public static AndroidDriver<?> driver;
+	public static AppiumDriver<?> driver;
 
-    public static  AndroidDriver<?> getDriver(){
+    public static  AppiumDriver<?> getDriver(){
         if(driver == null){
             conectar();
         }
         return driver;
     }
 
-    public static AndroidDriver<?> conectar() {
+    public static AppiumDriver<?> conectar() {
     	   try {
     	File diretorioAplicacao = new File("app");
         File arquivoAplicacao = new File(diretorioAplicacao, "Base.apk");
         DesiredCapabilities cap = new DesiredCapabilities();
         
-        cap.setCapability(MobileCapabilityType.PLATFORM_NAME, "ANDROID");
-        cap.setCapability(MobileCapabilityType.DEVICE_NAME, "sdk_gphone_x86");
+        cap.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID  );
+        cap.setCapability(MobileCapabilityType.DEVICE_NAME,  "sdk_gphone_x86");
         cap.setCapability(MobileCapabilityType.UDID, "emulator-5554");
         cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, "11");
         cap.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.ANDROID_UIAUTOMATOR2);
@@ -44,7 +47,7 @@ public class DriverFactory {
         cap.setCapability(MobileCapabilityType.APP,  arquivoAplicacao.getAbsolutePath());
 
         URL url = new URL("http://0.0.0.0:4723/wd/hub");
-            driver = new AndroidDriver<MobileElement>(url, cap);
+            driver = new AppiumDriver<MobileElement>(url, cap);
             
         } catch (MalformedURLException e) {
             e.printStackTrace();
@@ -53,7 +56,7 @@ public class DriverFactory {
         return driver;
     }
    
-	public static AndroidDriver<?> encerrarDriver() {
+	public static AppiumDriver<?> encerrarDriver() {
 		if (driver!= null) {
 			driver.quit();
 		}
