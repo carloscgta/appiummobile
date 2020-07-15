0abfd39eb16e2e2f32a01040546d810575eba3dd
Carlos Almeida <60942979+carlosteixeira89@users.noreply.github.com>
2020-07-14 23:35:39 -0300

    Refactory pom.xml dependencias

--- src/test/java/br/com/icarrositau/appium/runner/RunnerConsultaAppiCarros.java
+++ src/test/java/br/com/icarrositau/appium/runner/RunnerConsultaAppiCarros.java
@@ -3,12 +3,11 @@ package br.com.icarrositau.appium.runner;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
-import io.cucumber.junit.Cucumber;
+import cucumber.api.junit.Cucumber;
 
-//plugin = {"com.cucumber.listener.ExtentCucumberFormatter:src/main/report/cucumber-reports/report.html"},
 @RunWith(Cucumber.class)
 @CucumberOptions(glue = { " br.com.icarrositau.appium.steps", " br.com.icarrositau.appium.hooks"}, 
- features = {"./src/main/features/" }, 
+ features = {"./src/test/resources/features/" }, 
 tags = { "@lerListaDeResultadoValidarValorAVista" })
 public class RunnerConsultaAppiCarros {
 
