6a9aaf6d2d611e6452268e9f9a9bc59fe34f0b34
Carlos Almeida <60942979+carlosteixeira89@users.noreply.github.com>
2020-07-16 02:15:24 -0300

    Ajustes na Base Screen

--- src/main/java/br/com/icarrositau/appium/screens/BaseScreen.java
+++ src/main/java/br/com/icarrositau/appium/screens/BaseScreen.java
@@ -1,59 +1,59 @@
-//package br.com.icarrositau.appium.screens;
-//
-//import org.openqa.selenium.Keys;
-//import org.openqa.selenium.WebElement;
-//
-//import io.appium.java_client.AppiumDriver;
-//import io.appium.java_client.MobileElement;
-//import io.appium.java_client.android.AndroidDriver;
-//
-//public class BaseScreen extends SceenGenerator{
-//
-//
-//	
-//	public BaseScreen( AppiumDriver<MobileElement> driver) {
-//		// TODO Auto-generated constructor stub
-//		super(driver);
-//	}
-//
-//	
-//	public <T> void click(T elementAttr) throws InterruptedException {
-//
-//		waitForElement(elementAttr);
-//		((WebElement) elementAttr).click();
-//
-//	}
-//	
-//	public <T> void pressionarEnter(T elementAttr) throws InterruptedException {
-//
-//		waitForElement(elementAttr);
-//		((WebElement) elementAttr).sendKeys(Keys.ENTER);
-//
-//	}
-//	
-//	// Wait generic method
-//
-//	public <T> void waitForElement(T elementAttr) throws InterruptedException {
-//		
-//	}
-//
-//	public <T> Boolean elementIsDisplayed(T elementAttr) throws InterruptedException {
-//		Boolean  isDisplayed=false;
-//		if(((WebElement) elementAttr).isDisplayed()) {
-//			 isDisplayed=true;
-//		}
-//		
-//		return isDisplayed;
-//	}
-//
-//	
-//	// Write Text by using JAVA Generics (You can use both By or Webelement)
-//
-//	public <T> void writeText(T elementAttr, String text) throws InterruptedException {
-//		
-//		((WebElement) elementAttr).sendKeys(text);
-//
-//	}
-//	
-//	
-//}
+package br.com.icarrositau.appium.screens;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+
+public class BaseScreen extends SceenGenerator{
+
+
+	
+	public BaseScreen( AppiumDriver<MobileElement> driver) {
+		// TODO Auto-generated constructor stub
+		super(driver);
+	}
+
+	
+	public <T> void click(T elementAttr) throws InterruptedException {
+
+		waitForElement(elementAttr);
+		((WebElement) elementAttr).click();
+
+	}
+	
+	public <T> void pressionarEnter(T elementAttr) throws InterruptedException {
+
+		waitForElement(elementAttr);
+		((WebElement) elementAttr).sendKeys(Keys.ENTER);
+
+	}
+	
+	// Wait generic method
+
+	public <T> void waitForElement(T elementAttr) throws InterruptedException {
+		
+	}
+
+	public <T> Boolean elementIsDisplayed(T elementAttr) throws InterruptedException {
+		Boolean  isDisplayed=false;
+		if(((WebElement) elementAttr).isDisplayed()) {
+			 isDisplayed=true;
+		}
+		
+		return isDisplayed;
+	}
+
+	
+	// Write Text by using JAVA Generics (You can use both By or Webelement)
+
+	public <T> void writeText(T elementAttr, String text) throws InterruptedException {
+		
+		((WebElement) elementAttr).sendKeys(text);
+
+	}
+	
+	
+}
--- src/main/java/br/com/icarrositau/appium/screens/SceenGenerator.java
+++ src/main/java/br/com/icarrositau/appium/screens/SceenGenerator.java
@@ -1,44 +1,37 @@
-//package br.com.icarrositau.appium.screens;
-//
-//import org.openqa.selenium.support.PageFactory;
-//
-//import br.com.icarrositau.appium.core.DriverFactory;
-//import io.appium.java_client.AppiumDriver;
-//import io.appium.java_client.MobileElement;
-//import io.appium.java_client.android.AndroidDriver;
-//import io.appium.java_client.pagefactory.AppiumFieldDecorator;
-//
-//public class SceenGenerator extends DriverFactory{
-//
-//	public SceenGenerator(AndroidDriver<MobileElement> driver) {
-//		// TODO Auto-generated constructor stub
-//		this.driver= driver;
-//	}
-//	
-//	
-////	PageFactory.initElements(new AppiumFieldDecorator(getDriver()), this)
-//	
-//
-//	
-//	public  <TPage extends BaseScreen> TPage GetInstance (Class<TPage> Pageclass) throws Exception {
-//
-//        try {
-//
-//           //Initialize the Page with its elements and return it.
-//        	
-////        	return PageFactory.initElements(new AppiumFieldDecorator(getDriver()), Pageclass);
-//
+package br.com.icarrositau.appium.screens;
+
+import org.openqa.selenium.support.PageFactory;
+
+import br.com.icarrositau.appium.core.DriverFactory;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+
+public class SceenGenerator extends DriverFactory{
+
+	public SceenGenerator(AppiumDriver<MobileElement> driver) {
+		// TODO Auto-generated constructor stub
+		this.driver= driver;
+	}
+	
+
+	public  <TPage extends BaseScreen> void GetInstance (Class<TPage> Pageclass) throws Exception {
+
+        try {
+
+        	
+        	PageFactory.initElements(new AppiumFieldDecorator(getDriver()), Pageclass);
+
 //            return PageFactory.initElements(driver, Pageclass);
-//            		
-//            		//PageFactory.initElements(driver,  Pageclass);
-//
-//        } catch (Exception e) {
-//
-//            e.printStackTrace();
-//            
-//            throw e;
-//
-//        }
-//
-//    }
-//}
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+            
+            throw e;
+
+        }
+
+    }
+}
